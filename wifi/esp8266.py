b0594d2dfd726dd28f5c8648bb6549cb34eb08af
Aaron Watkins <ac.watkins80@gmail.com>
2021-12-04 17:01:59 -0500

    split and parse bytes into string after receive

--- wifi/esp8266.py
+++ wifi/esp8266.py
@@ -407,7 +407,8 @@ class ESP8266:
         retData = self._sendToESP8266("AT+CIPSTATUS\r\n")
         if retData:
             if "+CIPSTATUS" in retData:
-                if "2" in retData:
+                output_str = retData.split(":")[1].decode("utf-8")
+                if "2" in output_str:
                     return ESP8266_WIFI_CONNECTED
                 else:
                     return ESP8266_WIFI_DISCONNECTED
